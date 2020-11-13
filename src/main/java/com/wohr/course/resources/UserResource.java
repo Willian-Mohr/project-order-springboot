21578c5280a3e99d98dd2433f04c8a33f7b5a512
Willian Mohr <willianbmohr@gmail.com>
2020-11-13 11:26:51 -0300

    User delete

--- src/main/java/com/wohr/course/resources/UserResource.java
+++ src/main/java/com/wohr/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,20 +36,25 @@ public class UserResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
-	
-	/*{
-	 "name": "Bob Brown",
-	 "email": "bob@gmail.com",
-	 "phone": "977557755",
-	 "password": "123456"
-	}*/
+	/*
+	 * { "name": "Bob Brown", "email": "bob@gmail.com", "phone": "977557755",
+	 * "password": "123456" }
+	 */
 	@PostMapping
 	public ResponseEntity<User> insert(@RequestBody User obj) {
 		obj = service.insert(obj);
-		// Retorna o status 201 created ao invés de 200 OK e retorna o caminho do recurso criado
+		// Retorna o status 201 created ao invés de 200 OK e retorna o caminho do
+		// recurso criado
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
 
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		//Retorna o status 204 ok pois é uma reposta que não tem conteúdo
+		return ResponseEntity.noContent().build();
 	}
 
 }
