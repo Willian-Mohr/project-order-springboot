a0054a917370b9a3b4a89e0631738d6c1ac72e35
Willian Mohr <willianbmohr@gmail.com>
2020-11-13 10:21:38 -0300

    Payment, one-to-one association

--- src/main/java/com/wohr/course/config/TestConfig.java
+++ src/main/java/com/wohr/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.wohr.course.entities.Category;
 import com.wohr.course.entities.Order;
 import com.wohr.course.entities.OrderItem;
+import com.wohr.course.entities.Payment;
 import com.wohr.course.entities.Product;
 import com.wohr.course.entities.User;
 import com.wohr.course.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner {
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
