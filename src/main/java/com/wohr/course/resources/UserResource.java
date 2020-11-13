dcdab68843bf45f8bddf0d924b8d2ca7095ea02c
Willian Mohr <willianbmohr@gmail.com>
2020-11-13 11:38:32 -0300

    User update

--- src/main/java/com/wohr/course/resources/UserResource.java
+++ src/main/java/com/wohr/course/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,8 +54,20 @@ public class UserResource {
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
-		//Retorna o status 204 ok pois é uma reposta que não tem conteúdo
+		// Retorna o status 204 ok pois é uma reposta que não tem conteúdo
 		return ResponseEntity.noContent().build();
+	}
+
+	
+	/*{
+	 "name": "Bob Brown",
+	 "email": "bob@gmail.com",
+	 "phone": "977557755"
+	 }*/
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
