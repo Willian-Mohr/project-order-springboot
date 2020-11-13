fe7e84f33d53b152e14884531cff33e9173eef8b
Willian Mohr <willianbmohr@gmail.com>
2020-11-13 10:52:31 -0300

    User insert

--- src/main/java/com/wohr/course/resources/UserResource.java
+++ src/main/java/com/wohr/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.wohr.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.wohr.course.entities.User;
 import com.wohr.course.services.UserService;
@@ -29,6 +33,22 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	
+	/*{
+	 "name": "Bob Brown",
+	 "email": "bob@gmail.com",
+	 "phone": "977557755",
+	 "password": "123456"
+	}*/
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		// Retorna o status 201 created ao invés de 200 OK e retorna o caminho do recurso criado
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+
 	}
 
 }
